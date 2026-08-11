269021401f1654e3ad565387755c93781a0fa593
stalingYZFR6 <stalinggosling@gmail.com>
2026-08-11 12:23:21 -0600

    propietario

--- controllers/gato.controller.js
+++ controllers/gato.controller.js
@@ -2,32 +2,37 @@ import db from "../firebase.js";
 
 export const registrarGato = async (req, res) => {
   try {
-    const { nombre, edad, peso, raza } = req.body;
+    const { nombre, propietario, edad, peso, raza } = req.body;
 
-    if (!nombre || !edad || !peso || !raza) {
+    if (!nombre || !propietario || !edad || !peso || !raza) {
       return res.status(400).json({
-        mensaje: "Todos los campos son obligatorios: nombre, edad, peso y raza.",
+        mensaje:
+          "Todos los campos son obligatorios: nombre, propietario, edad, peso y raza.",
       });
     }
 
     // Si la colección "gatos" no existe, Firestore la crea automáticamente
     const docRef = await db.collection("gatos").add({
       nombre,
+      propietario,
       edad: Number(edad),
       peso: Number(peso),
       raza,
       fecha: new Date().toISOString(),
     });
 
-    const mensaje = `¡Gato registrado con éxito en Firebase! 
-ID: ${docRef.id} | Nombre: ${nombre} | Edad: ${edad} años | Peso: ${peso} kg | Raza: ${raza}`;
+    const mensaje = `¡Gato registrado con éxito en Firebase!
+ID: ${docRef.id} | Nombre: ${nombre} | Propietario: ${propietario} | Edad: ${edad} años | Peso: ${peso} kg | Raza: ${raza}`;
 
     res.json({ mensaje });
   } catch (error) {
     console.error(error);
+
     res.status(500).json({
       mensaje: "Error al guardar en Firebase: " + error.message,
     });
   }
 };
+
+
 
